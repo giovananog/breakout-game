94b6b05b6d376f28e336954dc1baa1fccf456f5e
Giovana Nogueira <ongiovana2016@gmail.com>
2024-01-23 12:04:21 -0300

    feat: ball class started

---
+++ ball.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.shape("circle")
+        self.penup()
+        self.x_move = 3
+        self.y_move = 3
+        self.move_speed = 0.1
+        self.goto(1, -289)
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+    
+    def reset_position(self):
+        self.goto(1, -289)
+        self.move_speed = 0.1
+        self.bounce_x()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from player import Player
 from scoreboard import Scoreboard
+from ball import Ball
 
 screen = Screen()
 
@@ -11,12 +12,17 @@ screen.title("Breakout Game")
 
 player = Player((0, -290))
 scoreboard = Scoreboard()
+ball = Ball()
 
 # ------------------- listeners
 screen.listen()
 screen.onkeypress(player.go_left, "a")
 screen.onkeypress(player.go_right, "d")
 
+# game loop
+game_is_on = True
+while game_is_on:
+    ball.move()
 
 
 
